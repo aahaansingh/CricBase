fa5b92b9c2bc6904141105b7efbca253193692c4
aahaansingh <147017461+aahaansingh@users.noreply.github.com>
2024-06-11 15:04:54 -0700

    functional notebook code for delivery storage, scorecard generation (very slow)

---
+++ data_read.py
@@ -0,0 +1,13 @@
+import pandas as pd 
+import numpy as np 
+import os
+import glob
+import json
+
+# def _init_ (self, path) :
+#     self.path = path
+#     self.match = self.match_data()
+#     (self.player, self.player_match) = self.player_data()
+#     (self.delivery, self.extras, self.wickets, self.fielder_wickets) = self.delivery_data()
+#     self.player_match_data # uses delivery data to auto-populate some basic scorecard stats in player_match
+
